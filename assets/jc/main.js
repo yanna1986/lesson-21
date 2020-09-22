9f527af364ae62261b041689f22f5f8a9b0619d9
Yana Manko <yanna.manko@gmail.com>
2020-09-22 09:12:27 +0300

    JC

--- assets/jc/main.js
+++ assets/jc/main.js
@@ -1,153 +1,172 @@
-console.log('Sample JavaScript #3 HW #17');
+console.log('Sample JavaScript #4 HW #18');
+/* eslint-disable multiline-comment-style */
+let myLongStr = 'Lorem". ipsum, dolor! sit? amet: consectetur; adipisicing elit. Dolores quas alias animi inventore delectus quo non, fugit officiis a repellendus facere quae perferendis quos doloremque in, accusantium eum sint corrupti similique voluptatibus omnis mollitia id ex. Adipisci temporibus laborum fugit aperiam, minima recusandae nemo! Voluptas, fugiat sunt saepe dignissimos quam possimus vero voluptatibus quas commodi ipsa vitae, est error voluptate ex cum? Ab, numquam doloremque sunt id molestias explicabo tenetur? Corporis, quisquam voluptatem doloremque itaque est quod impedit, commodi illo eius dicta, enim reiciendis quidem minus tempora sapiente ratione. Repudiandae tempora officia voluptate nam cum dolore corrupti dolorem asperiores quisquam dicta, officiis distinctio ad possimus earum rerum ipsam veritatis enim voluptatum ea numquam doloremque deleniti sapiente velit maxime. Temporibus nostrum perspiciatis molestiae tempora, quo molestias numquam atque obcaecati unde quos itaque modi fugiat dolorem non rerum harum, esse, impedit voluptas minus? Deleniti atque reiciendis voluptate aut consequuntur blanditiis dolores dolorem magni adipisci eius unde, rerum explicabo labore molestias non et exercitationem mollitia iure. Est libero recusandae et, animi sunt a blanditiis consequuntur nemo iste laudantium quos sint ipsa ad possimus sequi dolor ipsum mollitia facilis? Officia quasi rerum atque esse voluptatum quae eos! Sequi eum repellat, molestiae enim soluta quos, tempora recusandae nulla facere quae assumenda harum modi laboriosam. Earum error aspernatur explicabo? Earum non, nesciunt distinctio quod dolores facere, placeat consectetur illum omnis eligendi ullam! Totam laudantium voluptates esse inventore molestias sapiente corrupti temporibus iure! Explicabo laudantium repellendus ullam laboriosam. Odit nostrum sit autem at laboriosam? Autem cupiditate facilis dolore nemo perferendis facere! Maiores iure voluptas obcaecati nihil. Rerum dignissimos fugit a dolorem earum soluta, ut at ullam repudiandae sunt, voluptatum fugiat reiciendis distinctio ipsum, neque magni mollitia laborum perferendis nihil voluptatem adipisci assumenda aperiam! Libero natus fuga fugiat corrupti recusandae vel asperiores earum qui explicabo doloremque, magni error expedita dolor quia eos omnis nostrum facilis fugit molestiae culpa placeat eius! Fuga cum obcaecati esse, inventore cumque quibusdam fugiat beatae? Delectus rerum vel repellat fugiat eius id doloribus. Suscipit hic, vel nobis optio obcaecati cupiditate ullam? Facere, vitae natus, reprehenderit debitis, ea reiciendis doloribus possimus repudiandae nihil accusamus iusto neque ex voluptate nam fugiat voluptates atque facilis sed. Vitae non iure placeat, nam doloribus temporibus consectetur esse distinctio sit tempore, corrupti vero amet odit vel nostrum voluptatum! Ipsum nihil omnis tenetur debitis, sit eligendi! Suscipit vitae quae nemo eveniet veritatis. Perspiciatis modi expedita placeat aut voluptate officiis atque quasi tenetur nulla animi! Minus omnis nesciunt beatae ullam itaque facilis a consequatur, at fugiat natus placeat inventore vitae, cumque quo ipsum? Deserunt suscipit velit dolor nemo quasi deleniti nisi, temporibus explicabo laboriosam, corrupti, ad quibusdam ratione praesentium. Quo totam, qui libero, id iusto, asperiores vitae dolorem perferendis minus distinctio animi repudiandae. Reprehenderit maxime eum asperiores velit praesentium nulla soluta minus at ad consequuntur, error deleniti non corrupti voluptatibus ullam repellat quod ipsa tempora ut! Nemo corrupti aut quia corporis odit maxime laudantium nam unde perspiciatis est, dicta perferendis debitis et quod deleniti! Distinctio, sapiente ducimus ipsam id cumque eveniet totam veritatis nisi ex quis amet quod, quisquam fugit est perferendis quia culpa consequatur eaque incidunt. Rem repudiandae voluptatibus adipisci ad soluta deserunt sapiente culpa saepe veritatis similique illo veniam ipsum ducimus nisi, iste est accusantium unde nesciunt laboriosam itaque beatae eius exercitationem neque quam. In aspernatur totam doloribus quisquam voluptatem commodi, nostrum reiciendis, esse maxime ipsa laudantium ut sed quos. Repudiandae odio sunt minus reprehenderit, laudantium aliquam ea aperiam, voluptas placeat nemo harum excepturi commodi nostrum perspiciatis aut iusto. Accusamus ad voluptatem eaque, distinctio accusantium unde voluptatum esse autem molestiae obcaecati numquam. Cumque sint, est temporibus, quia animi iste quo repudiandae, laudantium voluptatibus ducimus architecto. Vel doloribus obcaecati perspiciatis voluptatum voluptatibus molestias sit, sed reiciendis esse ea exercitationem delectus quos officiis aliquid porro architecto magni sint temporibus illo aspernatur reprehenderit deserunt deleniti doloremque? Aliquam, ut minima. Architecto et, magnam dignissimos nam animi facilis? Recusandae quis ex repellat, voluptatibus veritatis modi beatae ea provident? Excepturi eligendi laudantium beatae dolores eum, accusantium eveniet ipsa quidem voluptas repellendus illo veniam magnam saepe asperiores quos modi in necessitatibus officiis tenetur dolorem, minima voluptate similique praesentium! Necessitatibus sint, itaque impedit, laboriosam, ratione blanditiis at dolor quos aliquam nobis nemo amet quia. Nostrum sit nesciunt, illum deleniti, aliquid, aut ipsa sapiente officia molestias ipsam modi. Quod cupiditate vel quidem iure nemo? Quisquam beatae natus est non amet nostrum dicta reiciendis expedita. Consequatur quo pariatur voluptatem voluptatum sed velit molestiae nemo odio sequi in? Soluta laboriosam ipsam incidunt architecto pariatur, harum molestiae dignissimos quaerat ducimus, dolorem autem qui aliquam doloremque tenetur itaque optio fuga, iusto officia at? Esse blanditiis veritatis voluptas, similique, ipsum, voluptate illum beatae ducimus magni perferendis fuga ea? Est voluptatum culpa delectus voluptatibus doloribus recusandae porro veritatis, molestiae magni soluta, possimus quidem voluptas suscipit obcaecati dolores voluptatem neque, asperiores labore corrupti dicta. Consequatur eius, inventore autem doloremque recusandae quae! Neque facere laudantium blanditiis inventore temporibus nostrum praesentium aperiam perspiciatis dicta perferendis ullam ab impedit adipisci dolorem exercitationem dignissimos ipsum minus incidunt tempore in, illo placeat odio dolorum. Maiores sed est id officiis praesentium ratione cumque ut iste alias laboriosam. Repellendus deserunt ut optio exercitationem ab repudiandae ex doloremque nisi magni numquam accusantium recusandae nesciunt sint quisquam odio, eum aliquam et excepturi culpa praesentium nam laudantium soluta dolorem saepe? Dicta hic sint totam temporibus expedita iste velit, minus blanditiis similique! Nemo quam natus explicabo voluptas perspiciatis ex molestias blanditiis magnam quibusdam, consequatur culpa ducimus facere soluta accusamus porro ratione velit fuga eaque. Natus consequuntur odit dolore illo totam minima, cum explicabo expedita debitis aperiam, tempore ducimus eum inventore odio assumenda atque, recusandae error. Doloribus omnis voluptate fugit ad quam a, mollitia culpa commodi libero vero delectus corporis fuga blanditiis nesciunt aut porro incidunt, iure dolore placeat necessitatibus odio quaerat iste. Necessitatibus enim earum praesentium tenetur, temporibus pariatur recusandae perferendis reiciendis numquam consequuntur alias non. Debitis commodi quia, magni minus quaerat aliquam assumenda repudiandae doloribus consectetur ipsam sapiente animi! Dolorem commodi tempore, quos reprehenderit ducimus adipisci cum quia maiores tenetur ex modi quibusdam incidunt. Minima, amet?';
+let wordsList = (str, subStr) => {
+  let reg = new RegExp('\\.|,|\\?|!|:|;|"', 'gui');
+  let arr = str.
+    replace(reg, '').
+    toLowerCase().
+    split(' ').
+    filter((arrItem) => arrItem.indexOf(subStr) > -1);
+  let res = new Set();
 
-let counter = (function () {
-    let count = 0;
-    return function (num) {
-        count = num === undefined ? count : num;
-        return count++;
-    };
-}());
-console.log(counter());
-console.log(counter());
-console.log(counter(100));
-console.log(counter());
-console.log(counter(500));
-console.log(counter());
-console.log(counter(0));
-console.log(counter());
-
-
-let counting = (function () {
-    let count = 0;
-
-    return {
-        value(num) {
-            if (num !== undefined) count = num;
-
-            return count;
-        },
-        decrement() {
-            count--;
-        },
-        increment() {
-            count++;
-        }
-    };
-}());
-
-console.log(counting.value());
-counting.increment();
-counting.increment();
-counting.increment();
-console.log(counting.value());
-counting.decrement();
-counting.decrement();
-console.log(counting.value()); 
-console.log(counting.value(100)); 
-counting.decrement();
-console.log(counting.value()); 
-console.log(counting.value(200)); 
-counting.increment();
-console.log(counting.value()); 
-
-
-let myPrint = (a, b, res) => `${a}^${b}=${res}`;
-let myPow = (a, b, callback) => {
-  let pow = (x, n) => {
-    if (n !== 1) return x *= pow(x, n - 1);
-
-    return x;
-  };
-
-  return callback(a, b, pow(a, b));
-};
-
-console.log(myPow(3, 4, myPrint)); 
-console.log(myPow(2, 3, myPrint)); 
-
-
-function fullInfo() {
-    return `${this.name} ${this.model}, ${this.engine}cc, year ${this.year}, ${this.used}`;
-  }
-  
-  let yearNow = new Date().getFullYear();
-  let car = {
-    engine: 2000,
-    model: 'Lacetti',
-    name: 'Chevrolet',
-    year: 2010,
-    info: fullInfo,
-    get used() {
-      return this.year !== yearNow ? 'used' : 'new';
-    },
-    set used(value) {
-      if (value === 'new' && this.year < yearNow) this.year = yearNow;
-    }
-  };
-  let car2 = {
-    engine: 5000,
-    model: 'FX50 AWD',
-    name: 'Infinite',
-    year: 2019,
-    info: fullInfo,
-    get used() {
-      return yearNow - this.year ? 'used' : 'new';
-    },
-    set used(value) {
-      if (value === 'new' && this.year < yearNow) this.year = yearNow;
-    }
-  };
-  
-  console.log(car.info()); 
-  car.used = 'new';
-  console.log(car.info()); 
-  car.used = 'used';
-  console.log(car.info()); 
-  console.log(car2.info()); 
-  car.used = 'used';
-  console.log(car2.info()); 
-
-  let list = [12, 23, 100, 34, 56, 9, 233];
-let myMax = (arg) => Math.max.apply(Math, arg);
-console.log(myMax(list));
-
-
-function myMul(a, b) {
-    return a * b;
-  }
-let myDouble = myMul.bind(null, 2);
-let myTriple = myMul.bind(null, 3);
-
-console.log(myDouble(3)); 
-console.log(myDouble(4)); 
-console.log(myDouble(5)); 
-console.log(myTriple(3)); 
-console.log(myTriple(4)); 
-console.log(myTriple(5)); 
-
-let notUniqNums = [1, 1, 2, 3, 4, 5, 6, 7];
-let notUniqStrings = [
-  'Bob',
-  'Kate',
-  'Jhon',
-  'Tom',
-  'Jhon',
-  'Kate',
-  'Tom',
-  'Bob',
-  'Jhon',
-  'Tom'
-];
-
-let myUniq = (arr) => {
-  let set = new Set();
-
-  arr.forEach((val) => {
-    set.add(val);
+  arr.forEach((arrItem) => {
+    res.add(arrItem);
   });
 
-  return set;
+  return res;
 };
 
-console.log(myUniq(notUniqNums));
-console.log(myUniq(notUniqStrings));
+
+console.log(wordsList(myLongStr, 'lore'));
+console.log(wordsList(myLongStr, 'no'));
+console.log(wordsList(myLongStr, 'rep'))
+
+
+
+let myDate = new Date();
+let getLocalDate = (date, isSeconds = false, isISO = false) => {
+  const reg = new RegExp(':\\d{2}$', 'gui');
+  let res;
+
+  if (!isISO) res = isSeconds
+    ? date.toLocaleString()
+    : date.toLocaleString().replace(reg, '');
+  else {
+    const year = date.getFullYear();
+    const month =
+      date.getMonth() + 1 < 9 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+    const day = date.getDate() < 9 ? `0${date.getDate()}` : date.getDate();
+    const hour = date.getHours() < 9 ? `0${date.getHours()}` : date.getHours();
+    const minutes =
+      date.getMinutes() < 9 ? `0${date.getMinutes()}` : date.getMinutes();
+    const seconds =
+      date.getSeconds() < 9 ? `0${date.getSeconds()}` : date.getSeconds();
+
+    res = isSeconds
+      ? `${year}-${month}-${day}, ${hour}:${minutes}:${seconds}`
+      : `${year}-${month}-${day}, ${hour}:${minutes}`;
+  }
+
+  return res;
+};
+console.log(getLocalDate(myDate));
+console.log(getLocalDate(myDate, true)); 
+console.log(getLocalDate(myDate, false, true)); 
+console.log(getLocalDate(myDate, true, true));
+console.log(getLocalDate(new Date(123456))); 
+console.log(getLocalDate(new Date(123456), true)); 
+console.log(getLocalDate(new Date(123456), false, true)); 
+console.log(getLocalDate(new Date(123456), true, true)); 
+
+console.log(getLocalDate(new Date(123456)) === '01.01.1970, 03:02');
+console.log(getLocalDate(new Date(123456), true) === '01.01.1970, 03:02:03');
+console.log(getLocalDate(new Date(123456), false, true) === '1970-01-01, 03:02');
+console.log(getLocalDate(new Date(123456), true, true) === '1970-01-01, 03:02:03');
+console.log(getLocalDate(new Date(1999999123456)) === '18.05.2033, 06:18');
+console.log(getLocalDate(new Date(1999999123456), true) === '18.05.2033, 06:18:43');
+console.log(getLocalDate(new Date(1999999123456), false, true) === '2033-05-18, 06:18');
+console.log(getLocalDate(new Date(1999999123456), true, true) === '2033-05-18, 06:18:43');
+
+let getWeekDay = (d) => {
+  const date = new Date(d);
+  const days = [ 'воскресенье', 'понедельник', 'вторник','среда', 'четверг', 'пятница','суббота'];
+
+  return days[date.getDay()];
+};
+
+console.log(getWeekDay('2019-01-30')); 
+console.log(getWeekDay('2019-07-16')); 
+console.log(getWeekDay('2019-07-27')); 
+
+let getLocalDay = (d) => {
+  const date = new Date(d);
+  let day = date.getDay();
+
+  if (day === 0) day = 7;
+
+  return day;
+};
+
+console.log(getLocalDay('2019-07-16')); 
+console.log(getLocalDay('2019-07-25')); 
+console.log(getLocalDay('2019-07-27')); 
+
+
+let getDateAgo = (d, days) => {
+  const date = new Date(d);
+
+  date.setDate(date.getDate() - days);
+  return date.toLocaleString().replace(/(\d.*),\s+(\d.*)/gu, '$1');
+};
+console.log(getDateAgo('2019-01-29', 1)); 
+console.log(getDateAgo('2019-01-29', 2)); 
+console.log(getDateAgo('2019-01-29', 365));
+
+let Car = function (engine, model, name, year) {
+  this.engine = engine;
+  this.model = model;
+  this.name = name;
+  this.year = year;
+};
+
+Object.defineProperties(Car.prototype, {
+  used: {
+    get() {
+      const yearNow = new Date().getFullYear();
+
+      return yearNow - this.year > 1 ? 'used' : 'new';
+    },
+    set(value) {
+      const yearNow = new Date().getFullYear();
+
+      if (value === 'new' && this.year < yearNow) this.year = yearNow;
+    }
+  }
+});
+
+Car.prototype.info = function () {
+  return `${this.name} ${this.model}, ${this.engine}cc, year ${this.year}, ${this.used}`;
+};
+
+let car = new Car(2000, 'Lacetti', 'Chevrolet', 2010);
+let car2 = new Car(5000, 'FX50 AWD', 'Infinite', 2019);
+
+console.log(car.info()); 
+car.used = 'new';
+console.log(car.info()); 
+car.used = 'used';
+console.log(car.info()); 
+console.log(car2.info()); 
+car.used = 'used';
+console.log(car2.info()); 
+
+let testPerformance = (iterations, func) => {
+  let time = Date.now();
+
+  if (typeof func === 'function') for (let i = iterations; i--;) func();
+
+  return Date.now() - time;
+};
+
+
+function test1() {
+  let str = myLongStr;
+
+  while (str.indexOf('o') !== -1) str = str.replace('o', '');
+  while (str.indexOf('a') !== -1) str = str.replace('a', '');
+  while (str.indexOf('e') !== -1) str = str.replace('e', '');
+  while (str.indexOf('u') !== -1) str = str.replace('u', '');
+  while (str.indexOf('i') !== -1) str = str.replace('i', '');
+}
+
+
+function test2() {
+  const reg = new RegExp('[oaeui]', 'gui');
+
+  myLongStr.replace(reg, '');
+}
+
+console.log(testPerformance(100, test1));
+console.log(testPerformance(100, test2));
+console.log(testPerformance(100, 12345));
